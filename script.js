8f75b72e2384d5240c16ea43ffed91517820a2ed
Junayed Khan <khanjunayed257@gmail.com>
2025-07-16 23:23:16 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function showSection(sectionId) {
-  document.querySelectorAll('.section').forEach(sec => sec.classList.add('hidden'));
-  document.getElementById(sectionId).classList.remove('hidden');
-}
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-  document.querySelector('.menu').classList.toggle('hidden');
-});
